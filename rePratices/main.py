d81d38db9f27e905681c2f78835f24ee07decd88
jiechue <q0490q0490@163.com>
2023-01-06 14:24:35 +0800

    re practices

--- rePratices/main.py
+++ rePratices/main.py
@@ -14,3 +14,22 @@ for i in lst:
 # match,search start from scratch such as ^\d
 lst = re.match(r"\d+", "10086,我的电话是:11111").group()
 print(lst)
+
+# preload
+com = re.compile(r"\d+")
+print(com.findall("10086,110,119,aa1"))
+
+s = """
+<div class='a1'><span id='1'>one</span></div>
+<div class='a2'><span id='2'>two</span></div>
+<div class='a3'><span id='3'>three</span></div>
+<div class='a4'><span id='4'>four</span></div>
+<div class='a5'><span id='5'>five</span></div>
+"""
+
+# re.S: 匹配换行 #(?P<group name>)
+obj = re.compile(r"<div class='.*?'><span id='\d+'>(?P<aaa>.*?)</span></div>", re.S)
+
+result = obj.finditer(s)
+for it in result:
+    print(it.group("aaa"))
